c0b727802dbd5df6537ae8fee111adb237d990a6
void-alive <sja9284@gmail.com>
2025-04-29 16:37:48 +0900

    병합 완료

--- team3_final_project/src/App.jsx
+++ team3_final_project/src/App.jsx
@@ -1,21 +1,8 @@
-import KnhApp from "./KimNaHyun/KnhApp.jsx";
-import PreSetting from "./KimSangMin/PreSetting.jsx";
-import PreTime from "./KimSangMin/PreTime.jsx";
-import PreTimeSet from "./KimSangMin/PreTimeSet.jsx";
-import SjhApp from "./simJiHyun/SjhApp.jsx";
-import Main from "./JeongSeongYun/main.jsx";
-import MainContent from "./JeongSeongYun/layout/MainContent.jsx";
-import LastWaiting from "./simJiHyun/LastWaiting.jsx";
-import TodayWaiting from "./simJiHyun/TodayWaiting.jsx";
 import PastDateWaiting from "./JangDaJung/PastDateWaiting.jsx";
 
 function App() {
   return (
     <div>
-      {/*<h1>team3 final project</h1>*/}
-      {/*  <Banner/>*/}
-      {/*<button type={"button"} onClick={() => axiosTest()}>테스트</button>*/}
-      {/*<CeoMain />*/}
         <PastDateWaiting />
     </div>
   )
--- team3_final_project/src/main.jsx
+++ team3_final_project/src/main.jsx
@@ -2,13 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import "../src/JeongSeongYun/css/mobile.css"
-import App from "./App.jsx";
 
-import App from './App.jsx'
 import DajungApp from "./JangDaJung/DajungApp.jsx";
+import SjhApp from "./simJiHyun/SjhApp.jsx";
+import JksApp from "./JeonKangSan/JksApp.jsx";
+import KnhApp from "./KimNaHyun/KnhApp.jsx";
+import Main from "./JeongSeongYun/main.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <DajungApp/>
-  </StrictMode>,
+      {/*<DajungApp/>*/}
+      {/*<SjhApp/>*/}
+      {/*<JksApp/>*/}
+      {/*<KnhApp/>*/}
+      <Main/>
+  </StrictMode>
 )
