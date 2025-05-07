01fe57114ce866317f9091c02e2077e709a86c89
jksa9609 <jksa9609@naver.com>
2025-05-07 14:51:55 +0900

    DTO 테스트

--- team3_final_project_server/src/main/java/com/example/team3_final_project_server/JunKangSan/JKSController.java
+++ team3_final_project_server/src/main/java/com/example/team3_final_project_server/JunKangSan/JKSController.java
@@ -1,11 +1,26 @@
 package com.example.team3_final_project_server.JunKangSan;
 
+import com.example.team3_final_project_server.dto.RestaurantDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class JKSController {
 
   @Autowired
   private JKSService jksService;
+
+
+  @GetMapping("/")
+  @ResponseBody
+  public String test() {
+    List<RestaurantDTO> restaurants = jksService.getAllRestaurants();
+    System.out.println("테스트 DTO 확인: " + restaurants);
+    return "출력 완료";
+  }
 }
--- team3_final_project_server/src/main/java/com/example/team3_final_project_server/JunKangSan/JKSService.java
+++ team3_final_project_server/src/main/java/com/example/team3_final_project_server/JunKangSan/JKSService.java
@@ -1,4 +1,10 @@
 package com.example.team3_final_project_server.JunKangSan;
 
+import com.example.team3_final_project_server.dto.RestaurantDTO;
+
+import java.util.List;
+
 public interface JKSService {
+
+    List<RestaurantDTO> getAllRestaurants();
 }
