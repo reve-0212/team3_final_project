18911aead8d2940e6a7096225ed457697bc27b3f
void-alive <sja9284@gmail.com>
2025-05-12 15:13:18 +0900

    뭔가 바뀜

--- team3_final_project/src/KimNaHyun/components/StoreMap.jsx
+++ team3_final_project/src/KimNaHyun/components/StoreMap.jsx
@@ -1,13 +1,43 @@
-import {useEffect} from "react";
+import {useEffect, useMemo} from "react";
 import {Map, MapMarker, useKakaoLoader} from "react-kakao-maps-sdk";
+import useGeolocation from "../../stores/useGeolocation.jsx";
 
 function StoreMap(props) {
+  const {currentMyLocation} = useGeolocation();
   useKakaoLoader({appkey: import.meta.env.VITE_REACT_APP_KAKAO_MAP_API_KEY})
 
-  useEffect(() => {
-    console.log("lat : " + props.resLat)
-    console.log("lng : " + props.resLng)
-  }, []);
+  console.log("currentMyLocation")
+  console.log(currentMyLocation.lat)
+  console.log(currentMyLocation.lng)
+
+  console.log("storeLocation")
+  console.log(props.resLat)
+  console.log(props.resLng)
+
+  // 좌표 두개 가지고 km 구하기
+  const getDistance = (lat1, lng1, lat2, lng2) => {
+    const deg2rad = (deg) => {
+      return deg * (Math.PI / 180)
+    }
+
+    const earthMeter = 6378
+    const dLat = deg2rad(lat2 - lat1)
+    const dLon = deg2rad(lng2 - lng1)
+    const a = Math.sin(dLat / 2) ** 2 +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    return earthMeter * c;
+  }
+
+  const distance = useMemo(() =>
+      getDistance(currentMyLocation.lat, currentMyLocation.lng,
+        props.resLat, props.resLng),
+    [currentMyLocation.lat, currentMyLocation.lng,
+      props.resLat, props.resLng])
+
+  console.log("distance : " + distance.toFixed(2) + "km")
+
 
   return (
     <Map id="map"
