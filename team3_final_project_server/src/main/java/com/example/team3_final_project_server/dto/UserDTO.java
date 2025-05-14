1a1298b466831ba15640852a7359cce64c21a31c
void-alive <sja9284@gmail.com>
2025-05-14 09:24:14 +0900

    다정씨것도 합쳐서 다시 커밋

--- team3_final_project_server/src/main/java/com/example/team3_final_project_server/dto/UserDTO.java
+++ team3_final_project_server/src/main/java/com/example/team3_final_project_server/dto/UserDTO.java
@@ -1,68 +1,68 @@
-//package com.example.team3_final_project_server.dto;
-//
-//import lombok.*;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.io.Serializable;
-//import java.util.Collection;
-//import java.util.Collections;
-//import java.util.List;
-//
-//@Data
-//@Builder
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class UserDTO implements UserDetails {
-//  private int userIdx;
-//  private String userId;
-//  private String userPass;
-//  private String userNick;
-//  private String userGender;
-//  private int userAge;
-//  private String userCall;
-//  private String userEmail;
-//  private String role;
-//
-////  사장전용
-//  private String bsName;
-//  private String bsNumber;
-//
-//  @Override
-//  public Collection<? extends GrantedAuthority> getAuthorities() {
-//    return Collections.singleton(new SimpleGrantedAuthority(this.role));
-//  }
-//
-//  @Override
-//  public String getPassword() {
-//    return this.userPass;
-//  }
-//
-//  @Override
-//  public String getUsername() {
-//    return this.userId;
-//  }
-//
-//  @Override
-//  public boolean isAccountNonExpired() {
-//    return true;
-//  }
-//
-//  @Override
-//  public boolean isAccountNonLocked() {
-//    return true;
-//  }
-//
-//  @Override
-//  public boolean isCredentialsNonExpired() {
-//    return true;
-//  }
-//
-//  @Override
-//  public boolean isEnabled() {
-//    return true;
-//  }
-//}
+package com.example.team3_final_project_server.dto;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO implements UserDetails {
+  private int userIdx;
+  private String userId;
+  private String userPass;
+  private String userNick;
+  private String userGender;
+  private int userAge;
+  private String userCall;
+  private String userEmail;
+  private String role;
+
+//  사장전용
+  private String bsName;
+  private String bsNumber;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.singleton(new SimpleGrantedAuthority(this.role));
+  }
+
+  @Override
+  public String getPassword() {
+    return this.userPass;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.userId;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
